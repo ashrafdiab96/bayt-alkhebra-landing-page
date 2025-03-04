b33ca674b9c9ff1201998913f4898733abc19ecc
Ashraf Diab <141237904+ashrafdiablinktsp@users.noreply.github.com>
2025-03-05 01:19:29 +0200

    Fix routes issues

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,12 @@ use App\Http\Controllers\UserDataController;
 use App\Exports\UsersExport;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return view('landing');
 });
 
 Route::get('/users', [UserDataController::class, 'index'])->name('users.index');
+
+Route::post('/submit-form', [UserDataController::class, 'store'])->name('form.submit');
 
 Route::get('/users/export', function () {
     return Excel::download(new UsersExport, 'users.xlsx');
